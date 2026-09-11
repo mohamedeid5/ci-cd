0d99f644f8995e993ae32ebd91efbe4c8371f61f
mohamed eid <meid368@gmail.com>
2026-09-11 08:56:56 +0300

    deploy

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
 });
 
-Route::get('/test', function () {
+Route::get("/test", function () {
     return "test";
 });
